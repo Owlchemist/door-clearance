7ccdc07e98638bdec550d8eafed414b98ade5562
Owlchemist <owlchemist@protonmail.com>
2023-04-22 13:17:18 -0700

    v1.0.1

--- Source/Patches.cs
+++ Source/Patches.cs
@@ -1,7 +1,10 @@
 using HarmonyLib;
 using Verse;
+using Verse.AI;
 using RimWorld;
 using System.Collections.Generic;
+using System;
+using System.Reflection.Emit;
  
 namespace DoorClearance
 {
@@ -13,12 +16,50 @@ namespace DoorClearance
         {
             new Harmony("owlchemist.doorclearance").PatchAll();
             var list = DefDatabase<ThingDef>.AllDefsListForReading;
-            System.Type building_door = typeof(Building_Door);
+            Type building_door = typeof(Building_Door);
             for (int i = list.Count; i-- > 0;)
             {
                 var def = list[i];
-                if (def.thingClass == building_door || (def.building != null && def.building.soundDoorOpenManual != null)) doors.Add(def.shortHash);
+                if (def.thingClass == building_door || def.thingClass.IsSubclassOf(building_door) || def.thingClass.Name.Contains("DoorsExpanded")) doors.Add(def.shortHash);
             }
+        }
+    }
+    
+    [HarmonyPatch(typeof(HaulAIUtility), nameof(HaulAIUtility.HaulablePlaceValidator))]
+    class Patch_HaulAIUtility_HaulablePlaceValidator
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var method = AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.GetEdifice));
+            int offset = 0;
+            bool found = false;
+            //Identify where the index is
+            foreach (var instruction in instructions)
+            {
+                if (offset > 0 && offset-- == 1)
+                {
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_HaulAIUtility_HaulablePlaceValidator), nameof(Validate)));
+                    yield return instruction; //If not filtered then just transfer control to normal vanilla handling
+			        yield return new CodeInstruction(OpCodes.Ldc_I4_0); //Otherwise, push false to the return
+			        yield return new CodeInstruction(OpCodes.Ret);
+                    offset = -1;
+                    found = true;
+                    continue;
+                }
+                if (offset < 0 && offset-- > -6) continue; //Skip next 5 instructions, no longer needed
+                if (offset == 0 && instruction.opcode == OpCodes.Call && instruction.OperandIs(method))
+                {
+                    offset = 3;
+                }
+                yield return instruction;
+            }
+            if (!found) Log.Error("[Door Clearance] Patch_HaulAIUtility_HaulablePlaceValidator transpiler failed to find its target. Did RimWorld update?");
+        }
+        public static bool Validate(Building edifice)
+        {
+            if (edifice == null) return false;
+            if (Setup.doors.Contains(edifice.def.shortHash) || edifice is Building_Trap) return true;
+            return false;
         }
     }
     
